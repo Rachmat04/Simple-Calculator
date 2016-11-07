d1b5361450d348128bd754365dbf55628a273422
Rachmat Wahidi <Rachmat Wahidi@SAKURAKO-PC>
2016-11-07 15:56:06 +0700

    Add project files.

---
+++ Simple Calculator/Module1.vb
@@ -0,0 +1,18 @@
+﻿Module Module1
+
+    Sub Main()
+        Console.WriteLine("Kalkulator sederhana")
+        Console.WriteLine()
+
+        Console.WriteLine("Masukkan nilai pertama: ")
+        Dim nilai1 As Integer = Console.ReadLine()
+        Console.WriteLine("Masukkan nilai kedua: ")
+        Dim nilai2 As Integer = Console.ReadLine()
+        Console.WriteLine()
+        Dim nilai3 As Integer
+        nilai3 = nilai1 * nilai2
+        Console.WriteLine("Hasil perkalian nilai pertama dan kedua adalah " & nilai3)
+        Console.ReadLine()
+    End Sub
+
+End Module
