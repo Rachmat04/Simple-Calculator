4cb49bfd7b9afbe3c46622a14c845904b42115d8
Rachmat04 <rachmatwahidi.site@gmail.com>
2016-11-08 12:23:57 +0700

    Change Integer to Double variable type.

--- Simple Calculator/Module1.vb
+++ Simple Calculator/Module1.vb
@@ -6,29 +6,32 @@
             Console.WriteLine()
 
             Console.WriteLine("Masukkan nilai pertama: ")
-            Dim nilai1 As Integer = Console.ReadLine()
+            Dim nilai1 As Double = Console.ReadLine()
             Console.WriteLine("Masukkan nilai kedua: ")
-            Dim nilai2 As Integer = Console.ReadLine()
+            Dim nilai2 As Double = Console.ReadLine()
             Console.WriteLine()
-            Dim nilai3, nilai4, nilai5, nilai6 As Integer
+            Console.WriteLine("Menghitung ...")
+            Console.WriteLine()
+            Dim nilai3, nilai4, nilai5, nilai6, nilai7 As Double
+            nilai1 = Math.Round(nilai1)
+            nilai2 = Math.Round(nilai2)
             nilai3 = nilai1 * nilai2
             nilai4 = nilai1 + nilai2
             nilai5 = nilai1 - nilai2
             nilai6 = nilai1 / nilai2
+            nilai7 = nilai1 ^ nilai2
             Console.WriteLine("Hasil penjumlahan nilai pertama dan kedua adalah " & nilai4)
             Console.WriteLine("Hasil pengurangan nilai pertama dan kedua adalah " & nilai5)
             Console.WriteLine("Hasil perkalian nilai pertama dan kedua adalah " & nilai3)
             Console.WriteLine("Hasil pembagian nilai pertama dan kedua adalah " & nilai6)
             Console.WriteLine()
-            Console.WriteLine("Hasil dari nilai pertama pangkat nilai kedua adalah " & nilai1 ^ nilai2)
+            Console.WriteLine("Hasil dari nilai pertama pangkat nilai kedua adalah " & nilai7)
             Console.ReadLine()
 
         Catch ex As InvalidCastException
-            Console.WriteLine("Galat: Anda memasukkan karakter non-angka. Aplikasi akan ditutup.")
+            Console.WriteLine("Galat: Anda memasukkan karakter selain angka. Aplikasi akan ditutup. Silakan muat ulang aplikasi.")
         Catch Ex As Exception
             Console.WriteLine("Galat: " & Ex.Message)
-        Finally
-            Console.WriteLine("Silakan muat ulang aplikasi.")
             Console.ReadLine()
         End Try
     End Sub
